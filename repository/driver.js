fd9a983962a0d9897d3210b9df8fd9294fbfecbb
amimulamim <142148987+amimulamim@users.noreply.github.com>
2023-08-13 16:25:27 +0600

    Update driver.js

--- repository/driver.js
+++ repository/driver.js
@@ -13,7 +13,7 @@ class DriverRepository extends Repository{
 
     findOne = async function(id,id2){
         // Binding occurs serially as present in the array
-        const query = "SELECT * FROM employees WHERE employee_id > :employee_id AND employee_id < :2";
+        const query = "SELECT * FROM driver WHERE id=:id";
         const params = [id,id2];
         const result = await this.query(query,params);
         return result;
